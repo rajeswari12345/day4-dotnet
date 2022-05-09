00302c4cf4c03940e5c018b89b2d31074bd61d33
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 07:44:42 +0530

    day 4 uc2

---
+++ day4 uc2.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using  System;
+using  System.Collection.Generic;
+using  System.Linq;
+using  System.Text;
+using  System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    class Gameuc2
+    {
+        public static void player()
+        {
+            private Random rnd = new Random();
+        void MyMethod()
+        {
+            int dice1 = rnd.Next(1, 6); // creates a number between 1 and 6 from dice 1
+            int dice2 = rnd.Next(1, 6);   // creates a number between 1 and 6 from dice 2
+            int dice3 = rnd.Next(1, 6);  // creates a number between 1 and 6 from dice 3
+            
+        }
+    }
+}
+
+    
