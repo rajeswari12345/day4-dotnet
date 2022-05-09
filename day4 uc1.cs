9731a724dd8ba7d8b06d65260c95b81367be0ede
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 07:43:58 +0530

    day 4 uc1

---
+++ day4 uc1.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using  System;
+using  System.Collection.Generic;
+using  System.Linq;
+using  System.Text;
+using  System.Threading.Tasks;
+  
+namespace Snake_Ladder
+{
+    class Gameuc1
+    {
+        public static void player()
+        {
+            /* Snake and Ladder game played with single player at start position 0 with start position */
+            int Player_position = 0;
+            Console.WriteLine(" Lets begin the game");
+            Console.WriteLine(" number of player: single player");
+            Console.WriteLine(" player position [0] ,player position");
+
+
+        }
+    }
+}
+
+
+
